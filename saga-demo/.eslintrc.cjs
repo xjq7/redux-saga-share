ddb1c1659084028f8983927b14a582a741d4adbf
xjq <lebnizjoan@gmail.com>
2021-07-07 11:11:34 +0800

    u

--- saga-demo/.eslintrc.cjs
+++ saga-demo/.eslintrc.cjs
@@ -4,12 +4,10 @@ module.exports = {
     es2021: true,
   },
   globals: {
-    g: "writeable",
     module: "readonly"
   },
   parserOptions: {
     sourceType: "module",
-    ecmaVersion: 7,
     ecmaFeatures: {
       jsx: true,
       impliedStrict: true,
@@ -29,6 +27,18 @@ module.exports = {
     "no-extra-boolean-cast": 2,
     "no-extra-semi": 2,
     "no-func-assign": 1,
-    "no-inner-declarations": 1
+    "no-inner-declarations": 1,
+    "prefer-rest-params": 2,
+    "default-param-last": 2,
+    "no-param-reassign": 1,
+    "function-paren-newline": [2, { minItems: 6 }],
+    "arrow-parens": [2, "as-needed", { "requireForBlockBody": true }],
+    "arrow-body-style": [2, "as-needed"],
+    "no-duplicate-imports": 2,
+    "generator-star-spacing": [2, { before: false, after: true }],
+    "eqeqeq": [2, "smart"],
+    "no-unneeded-ternary": 2,
+    "no-mixed-operators": 2,
+    "nonblock-statement-body-position": 2
   },
 }
